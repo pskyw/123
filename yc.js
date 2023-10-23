eef045b2a3dc632d02bcb9bc1afde4fcffc32bd8
pskyw <148472469+pskyw@users.noreply.github.com>
2023-10-23 11:49:55 +0800

    Create yc.js

---
+++ yc.js
@@ -0,0 +1,13 @@
+let body = $response.body;
+
+// 邮储节日节气活动
+if ($request.url.includes('https://m.ujia007.com/ujia_mall/api/product/getProduct')) {
+  body = body.replace(/"status":\s*"\d"/g, '"status": "1"');
+}
+
+// 
+if ($request.url.includes('https://m.ujia007.com/ujia_mall/api/product/getProduct')) {
+  body = body.replace(/"stock":\s*"\d+"/g, '"stock": "1"');
+}
+
+$done({ body });
