bdcac7a86f07e5ee4359d9327fe2d93d1035211b
pskyw <148472469+pskyw@users.noreply.github.com>
2023-10-29 10:55:13 +0800

    Create bjyhz3.js

---
+++ bjyhz3.js
@@ -0,0 +1,8 @@
+let body = $response.body;
+
+// 北京银行周三
+if ($request.url.includes('https://h5api.xiaomyc.com/bob/')) {
+          body = body.replace(/"saleState":\s*\d/g, '"saleState": 1');
+}
+
+$done({ body });
