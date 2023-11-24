e238c3481cfdb641246bcf0c5efc8d623bf36f50
pskyw <148472469+pskyw@users.noreply.github.com>
2023-11-24 22:39:44 +0800

    Update piaoxingqiu.js

--- piaoxingqiu.js
+++ piaoxingqiu.js
@@ -1,9 +1,13 @@
 let body = $response.body;
 
-// 猫眼演唱会
-if ($request.url.includes('https://m.piaoxingqiu.com/cyy_gatewayapi/show/pub/v5/show')) {
+// 票星球
+if ($request.url.includes('https://m.piaoxingqiu.com/cyy_gatewayapi/show/')) {
   body = body.replace(/"canBuyCount":\s*0/g, '"canBuyCount":100');
 }
 
+// 
+if ($request.url.includes('https://m.piaoxingqiu.com/cyy_gatewayapi/show/')) {
+  body = body.replace("PENDING", "LACK_OF_TICKET");
+}
 
 $done({ body });
