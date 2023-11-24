5ebe9c260872aa4476928ff6d070ea75616b41d1
pskyw <148472469+pskyw@users.noreply.github.com>
2023-11-24 20:55:43 +0800

    Update maoyan.js

--- maoyan.js
+++ maoyan.js
@@ -2,12 +2,12 @@ let body = $response.body;
 
 // 猫眼演唱会
 if ($request.url.includes('https://yanchu.maoyan.com/myshow/ajax/v2/')) {
-  body = body.replace(/"showStatus":\s*\d/g, '"showStatus": 0');
+  body = body.replace(/"showStatus":\s*\d/g, '"showStatus":0');
 }
 
 // 
 if ($request.url.includes('https://yanchu.maoyan.com')) {
-  body = body.replace(/"code":\s*1500/g, '"code": 200');
+  body = body.replace(/"code":\s*1500/g, '"code":200');
 }
 
 $done({ body });
