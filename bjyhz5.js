64eb5810e910bf5d5799ec8d341bec1fc13dac55
pskyw <148472469+pskyw@users.noreply.github.com>
2024-08-09 10:49:30 +0800

    Update bjyhz5.js

--- bjyhz5.js
+++ bjyhz5.js
@@ -2,7 +2,7 @@ let body = $response.body;
 
 // bjyh周五影票
 if ($request.url.includes('https://msact-xshd-xt.bankofbeijing.com.cn:8088/ibbp-market-web-seckill/')) {
-  body = body.replace(/"userTakeNum":\s*\d/g, '"userTakeNum":10');
+  body = body.replace(/"userTakeNum":\s*\d+/g, '"userTakeNum":10');
 }
 // 
 if ($request.url.includes('https://msact-xshd-xt.bankofbeijing.com.cn:8088/ibbp-market-web-seckill/')) {
